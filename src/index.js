58ef43af208a11dfc300acbed1542c9b4a336476
tamana balkhi <tamana.balkhi1377@gmail.com>
2022-08-27 16:05:49 +0430

    receiving data from the API and parsing the JSON

--- src/index.js
+++ src/index.js
@@ -1,6 +1,38 @@
-import addScoresC from './modules/addScore.js';
-import sTable from './modules/scoretable.js';
-import './style.css';
 
-document.querySelector('main').innerHTML += addScoresC;
-document.querySelector('main').innerHTML += sTable;
+import './style.css';
+import display from './modules/displayScore.js';
+
+const refreshList = document.getElementById('refresh-btn');
+
+refreshList.addEventListener('click', display);
+
+const addScore = document.getElementById('add_score');
+addScore.addEventListener('click', (e) => {
+  e.preventDefault();
+  const name = document.getElementById('input-name');
+  const score = document.getElementById('input-score');
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/HM4EVKKEOdqP9F8CeQFM/scores', {
+    method: 'POST',
+    body: JSON.stringify({
+      user: name.value,
+      score: score.value,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  name.value = '';
+  score.value = '';
+});
+
+
+// Create the game
+/*fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({
+    name: 'lboard',
+  }),
+})
+  .then((res) => res.json())
+  .then((data) => console.log(data.result));*/
